2424323dc56e30d1e42002321eb196e7eb8d5855
Balaji Sivakumar <66515822+BalajiShivakumar@users.noreply.github.com>
2024-06-25 10:46:17 +0100

    Add files via upload

---
+++ switchCase/switchCase/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        
+        Console.WriteLine("Enter the first letter of your mobile brand:");
+        char input;
+        input = Console.ReadKey().KeyChar;
+        Console.WriteLine();  
+        input = char.ToLower(input);
+
+        
+        switch (input)
+        {
+            case 'o':
+                Console.WriteLine("One Plus");
+                break;
+            case 's':
+                Console.WriteLine("Samsung");
+                break;
+            case 'r':
+                Console.WriteLine("Realme");
+                break;
+            case 'v':
+                Console.WriteLine("Vivo");
+                break;
+            case 'i':
+                Console.WriteLine("Infinix");
+                break;
+            default:
+                Console.WriteLine("Unknown brand");
+                break;
+        }
+
+        Console.ReadKey();
+    }
+}
