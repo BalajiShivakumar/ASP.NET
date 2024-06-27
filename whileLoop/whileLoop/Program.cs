d7a823311b01936ff710df82c207bc2ecbe89c32
Balaji Sivakumar <66515822+BalajiShivakumar@users.noreply.github.com>
2024-06-27 21:48:34 +0100

    Add files via upload

---
+++ whileLoop/whileLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace whileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = 1;
+            Console.WriteLine("Printing all Numbers from 1 to 20");
+            while (n <= 20)
+            {   
+                Console.WriteLine(+n);
+                n++;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ whileLoopOdd/whileLoopOdd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Linq;
+namespace whileLoopOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            Console.WriteLine("Printing all odd numbers from 1 to 20");
+            while (n <= 20)
+            {
+                Console.WriteLine(+n);
+                n=n+2;
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
