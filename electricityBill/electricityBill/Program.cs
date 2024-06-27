77d3448eb3e5608ac171ccb0ad8e50592ccc5527
Balaji Sivakumar <66515822+BalajiShivakumar@users.noreply.github.com>
2024-06-27 21:47:54 +0100

    Add files via upload

---
+++ electricityBill/electricityBill/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Linq;
+
+namespace electricityBill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter the units used : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n <= 50)
+            {
+                n = n * 5;
+            }
+            else if (n <= 150 && n >= 51)
+            {
+                n = 250 + ((n-50) * 10);
+            }
+            else
+            {
+                n = 1250 + ((n - 150) * 15);
+            }
+            Console.WriteLine("Electricity Bill : " + n);
+            Console.ReadKey();
+        }
+    }
+}
