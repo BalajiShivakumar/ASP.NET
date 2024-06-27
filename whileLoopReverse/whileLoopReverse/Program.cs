8740abb56deabf38f05a2f4c3a0349ffcddd3922
Balaji Sivakumar <66515822+BalajiShivakumar@users.noreply.github.com>
2024-06-27 21:49:19 +0100

    Add files via upload

---
+++ whileLoopReverse/whileLoopReverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Linq;
+
+namespace whileLoopReverse
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int n;
+            Console.WriteLine("Enter the value of n: ");
+            n= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Printing all number from " + n + " to 0");
+            while (n >= 0)
+            {
+                Console.WriteLine(+n);
+                n--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ whileLoopTable/whileLoopTable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Linq;
+namespace whileLoopTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            while (n <= 20) 
+            {
+                Console.WriteLine(+n + " * 5 = " + (n * 5));
+                n++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
