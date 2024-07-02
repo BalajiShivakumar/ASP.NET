3a488292afe83a69ce41eb2e26e1090de48b4a09
Balaji Sivakumar <66515822+BalajiShivakumar@users.noreply.github.com>
2024-07-02 15:17:04 +0100

    Add files via upload

---
+++ fibonacci/fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+using System.Linq;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, a = 0, b = 1, c, n;
+            Console.WriteLine("Enter till which number the fibonacci series should be generated : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (i <= n)
+            {
+                Console.Write(a+",");
+                c = a + b;
+                a = b;
+                b = c;
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
