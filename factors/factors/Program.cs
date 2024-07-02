9cd96a57a00a10c6aae1cb41e76e0ddcc21b2335
Balaji Sivakumar <66515822+BalajiShivakumar@users.noreply.github.com>
2024-07-02 15:17:39 +0100

    Add files via upload

---
+++ factors/factors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Linq;
+
+namespace factors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            i = 1;
+            Console.WriteLine("Enter the number : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Factors are : ");
+            while (i < n)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write(i+" ,");
+                }
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
